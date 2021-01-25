a79d6a81ac901197d8d40c87d0b9e0cfa11ea48d
Leigh McCulloch <leigh@mcchouse.com>
2021-01-25 09:21:03 -0800

    Fix dir creation

--- install.sh
+++ install.sh
@@ -25,8 +25,9 @@ links=(
 for f in "${links[@]}"; do
   dest="$HOME/.$f"
   if [[ "$f" == *\/* ]]; then
-    echo "Creating directory $dest..."
-    mkdir -m 700 -p "$dest"
+    destdir="$(dirname $dest)"
+    echo "Creating directory $destdir..."
+    mkdir -m 700 -p "$destdir"
   fi
   echo "Linking $PWD/files/$f at $dest..."
   ln -sf "$PWD/files/$f" "$dest"
