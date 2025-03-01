22b72cea1bef8ec51e08c070d7a27271e60bfbae
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-03-01 15:05:59 +1000

    fix vimrc.lock pinning with plugupdate remove

--- update-vim-plugins.sh
+++ update-vim-plugins.sh
@@ -7,7 +7,11 @@ set -o nounset
 # update vim-plug
 curl -fLo autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+git diff -- autoload/plug.vim
+
 # update vim plugins
 rm -f vimrc.lock
 vim +PlugUpdate '+PlugSnapshot vimrc.lock' +qa
-sed -i '/^PlugUpdate/d' vimrc.lock
+sed -i.bak '/^PlugUpdate/d' vimrc.lock
+
+git diff -- autoload/plug.vim
