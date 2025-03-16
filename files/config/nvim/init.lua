43f53e9975224ed86926201bd64054d6692fcc85
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-03-16 23:15:56 +1000

    add lsp configuration and dependencies to neovim

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -3,3 +3,8 @@ set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vim/vimrc
 ]])
+
+require'lspconfig'.rust_analyzer.setup{}
+require'lspconfig'.gopls.setup{}
+require'lspconfig'.denols.setup{}
+require'lspconfig'.lua_ls.setup {}
