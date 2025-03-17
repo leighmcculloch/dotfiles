90f4a80b5f170c94554676b9c8b647897bdbdd56
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-03-17 14:01:26 +1000

    configure neovim autocompletion and theme changes

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -4,13 +4,76 @@ let &packpath = &runtimepath
 source ~/.vim/vimrc
 ]])
 
-require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.gopls.setup{}
-require'lspconfig'.denols.setup{}
-require'lspconfig'.lua_ls.setup {}
-
 require('lualine').setup{
   options = {
-    theme = 'auto'
+    theme = 'sonokai',
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        newfile_status = false,
+        path = 1,
+        shorting_target = 40,
+        symbols = {
+          modified = '[+]',
+          readonly = '[-]',
+          unnamed = '[No Name]',
+          newfile = '[New]',
+        }
+      },
+    },
+    lualine_x = {
+      {
+        'lsp_status',
+        icon = '',
+        symbols = {
+          spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+          done = '✓',
+          separator = ' ',
+        },
+        ignore_lsp = {'GitHub Copilot'},
+      },
+      'filetype',
+    },
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
   }
+}
+
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-q>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({ { name = 'nvim_lsp' } })
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require'lspconfig'.rust_analyzer.setup{
+  capabilities = capabilities,
+}
+require'lspconfig'.gopls.setup{
+  capabilities = capabilities,
+}
+require'lspconfig'.denols.setup{
+  capabilities = capabilities,
+}
+require'lspconfig'.lua_ls.setup {
+  capabilities = capabilities,
 }
