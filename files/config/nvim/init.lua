cf9c14e9a0c86a611b388b29f4ae59f9cbbdfd3d
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-04-10 22:48:58 +1000

    improve nvim-tree float configuration with dynamic sizing

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -103,16 +103,29 @@ require("nvim-tree").setup {
     signcolumn = 'no',
     float = {
       enable = true,
-      quit_on_focus_loss = true,
-      open_win_config = {
-        relative = "editor",
-        border = "rounded",
-        width = 30,
-        height = 30,
-        row = 2,
-        col = 3,
-      },
+      open_win_config = function()
+        -- From https://github.com/MarioCarrion/videos/blob/269956e913b76e6bb4ed790e4b5d25255cb1db4f/2023/01/nvim/lua/plugins/nvim-tree.lua
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * 0.5
+        local window_h = screen_h * 0.8
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+        return {
+          border = "rounded",
+          relative = "editor",
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
     },
+    width = function()
+      return math.floor(vim.opt.columns:get() * 0.5)
+    end,
   },
   on_attach = function(bufnr)
     local api = require "nvim-tree.api"
