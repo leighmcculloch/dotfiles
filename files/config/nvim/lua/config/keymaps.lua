8545cc4b073430a39ab2e98aa6b0017998c14d52
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-04-14 13:08:56 +1000

    add typescript support and auto-save functionality

--- files/config/nvim/lua/config/keymaps.lua
+++ files/config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<c-h>", "<s-h>", { remap = true, desc = "Change to left buffer" })
+map("n", "<c-l>", "<s-l>", { remap = true, desc = "Change to right buffer" })
+
+map("n", "<c-p>", "<leader> ", { remap = true, desc = "Open file picker" })
+map("n", "<c-q>", "<leader>bd", { remap = true, desc = "Close buffer" })
+map("n", "<c-e>", "<leader>e", { remap = true, desc = "Reveal file in file explorer" })
+--map("n", "<c-e>", function()
+--  if vim.bo.filetype == "snacks_picker_list" then
+--    vim.cmd("close")
+--  else
+--    Snacks.explorer.reveal()
+--  end
+--end, { remap = true, desc = "Reveal file in file explorer" })
+
+map("n", "gk", function()
+  local on = not vim.diagnostic.config().virtual_lines
+  vim.diagnostic.config({ virtual_lines = on })
+end, { desc = "Toggle diagnostic virtual lines" })
--- files/config/nvim/lua/config/lazy.lua
+++ files/config/nvim/lua/config/lazy.lua
@@ -30,9 +30,9 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "tokyonight" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
+    enabled = false, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
