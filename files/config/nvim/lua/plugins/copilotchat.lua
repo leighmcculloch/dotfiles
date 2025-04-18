b8ccf65be8f44d0959a317f3cce56f18f6e0e06d
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-04-18 23:27:04 +1000

    add copilotchat and snacks dashboard plugins

---
+++ files/config/nvim/lua/plugins/copilotchat.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      auto_insert_mode = false,
+
+      agent = "copilot",
+
+      --model = "claude-3.7-sonnet",
+      --model = "gpt-4.1",
+      model = "gemini-2.5-pro",
+    },
+  },
+}
