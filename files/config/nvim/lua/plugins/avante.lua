bf81017525cc9e7edd1023b190ca2c289512aa8c
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-04-21 23:39:04 +1000

    replace CopilotChat with avante.nvim integration

---
+++ files/config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,90 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "ibhagwan/fzf-lua",
+    },
+    opts = {
+      hints = { enabled = false },
+
+      ---@alias AvanteProvider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+      provider = "gemini",
+      auto_suggestions_provider = "gemini",
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-7-sonnet-20250219",
+        temperature = 0,
+        max_tokens = 4096,
+      },
+      gemini = {
+        endpoint = "https://gemini.research.google.com",
+        model = "gemini-2.5-pro",
+        temperature = 0,
+        max_tokens = 4096,
+      },
+
+      --- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string
+      file_selector = {
+        provider = "snacks",
+        provider_opts = {},
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    lazy = true,
+    dependencies = { "saghen/blink.compat" },
+    opts = {
+      sources = {
+        default = { "avante_commands", "avante_mentions", "avante_files" },
+        compat = {
+          "avante_commands",
+          "avante_mentions",
+          "avante_files",
+        },
+        -- LSP score_offset is typically 60
+        providers = {
+          avante_commands = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 90,
+            opts = {},
+          },
+          avante_files = {
+            name = "avante_files",
+            module = "blink.compat.source",
+            score_offset = 100,
+            opts = {},
+          },
+          avante_mentions = {
+            name = "avante_mentions",
+            module = "blink.compat.source",
+            score_offset = 1000,
+            opts = {},
+          },
+        },
+      },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    optional = true,
+    ft = function(_, ft)
+      vim.list_extend(ft, { "Avante" })
+    end,
+    opts = function(_, opts)
+      opts.file_types = vim.list_extend(opts.file_types or {}, { "Avante" })
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>a", group = "ai" },
+      },
+    },
+  },
+}
