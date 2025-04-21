bb5f3010f31a7c3268a979c624b6d462e3cd92d4
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-04-21 23:14:40 +1000

    dashboard wip

--- files/config/nvim/lua/plugins/snacks-dashboard.lua
+++ files/config/nvim/lua/plugins/snacks-dashboard.lua
@@ -3,68 +3,28 @@ return {
   opts = {
     dashboard = {
       enabled = true,
+
+      width = 120,
       preset = {
         header = [[
-
-████████╗██╗░░██╗███████╗  ███████╗███╗░░██╗██████╗░
-╚══██╔══╝██║░░██║██╔════╝  ██╔════╝████╗░██║██╔══██╗
-░░░██║░░░███████║█████╗░░  █████╗░░██╔██╗██║██║░░██║
-░░░██║░░░██╔══██║██╔══╝░░  ██╔══╝░░██║╚████║██║░░██║
-░░░██║░░░██║░░██║███████╗  ███████╗██║░╚███║██████╔╝
-░░░╚═╝░░░╚═╝░░╚═╝╚══════╝  ╚══════╝╚═╝░░╚══╝╚═════╝░
-
-   ██╗░██████╗  ███╗░░██╗███████╗░█████╗░██████╗░
-   ██║██╔════╝  ████╗░██║██╔════╝██╔══██╗██╔══██╗
-   ██║╚█████╗░  ██╔██╗██║█████╗░░███████║██████╔╝
-   ██║░╚═══██╗  ██║╚████║██╔══╝░░██╔══██║██╔══██╗
-   ██║██████╔╝  ██║░╚███║███████╗██║░░██║██║░░██║
-   ╚═╝╚═════╝░  ╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝]],
+        ]],
           -- stylua: ignore
           ---@type snacks.dashboard.Item[]
           keys = {
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            { icon = " ", key = "b", desc = "Browse", action = ":lua Snacks.gitbrowse()" },
+            --{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            --{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            --{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            --{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            --{ icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            --{ icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+            --{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            --{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
           },
       },
       sections = {
         { section = "header" },
-        {
-          pane = 2,
-          desc = [[
-
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢉⣭⣭⣭⣭⣭⣭⣙⣛⠻⢿⣿⣿⣿⣿⣿⡇⢀⣀⡀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡌⠻⢿⣿⣿⣿⣿⠏
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⠿⢿⡿⢋⣽⣿⣿⣿⣿⣿⡿⠉⣐⠲⠖⣿⣿⣿⣿⡻⠿⠓
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢴⡿⠖⠀⣭⠉⣛⠿⣿⣿⣿⣿⣿⠟⣡⣾⣿⣿⣿⣿⣧
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣶⣶⣿⣷⣝⠿⢿⡿⠆⣹⣿⡟⣡⣾⣿⣿⣿⣿⣿⣿⣿
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣉⣛⣻⣿⣿⣿⣿⣿⣶⣶⣿⣿⡿⢰⠟⠹⠿⣿⣿⣿⣿⣿⠏
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⢶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣠⣊⣉⢣⢹⣿⣿⠟⠁
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⢀⠀⣀⣀⠀⣀⣀⠀⣤⡀⠀⢶⣶⣮⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠻⣭⡡⠊⠘⠉
-⠠⠰⣶⣄⠠⣶⣦⡐⢶⠿⠦⠙⠿⠿⠶⠍⠻⠛⠀⠉⠉⠈⠛⠃⠈⠁⠀⠚⠋⠉⠴⣿⣿⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁
-⠀⠑⠈⠁⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⣬⣽⣿⣿⣿⣿⣿⣿⣿⣿⠁
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⠃
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⠃
-
-]],
-        },
         { section = "keys", gap = 1, padding = 1 },
-        {
-          pane = 2,
-          icon = " ",
-          desc = "Browse Repo",
-          padding = 1,
-          key = "b",
-          action = function()
-            Snacks.gitbrowse()
-          end,
-        },
         function()
           local in_git = Snacks.git.get_root() ~= nil
           local cmds = {
@@ -76,13 +36,23 @@ return {
               end,
               key = "n",
               icon = " ",
-              height = 5,
+              height = 7,
               enabled = true,
               cache = true,
             },
             {
               title = "Open Issues",
-              cmd = "gh issue list -L 3",
+              cmd = [[
+                GH_PAGER= gh issue list -L 7 \
+                --search 'sort:updated-desc assignee:@me' \
+                --json number,title,assignees,updatedAt \
+                --template '{{range .}}{{tablerow .number .title (join "," (pluck "login" .assignees)) (timeago .updatedAt)}}{{end}}{{tablerender}}' \
+                && \
+                GH_PAGER= gh issue list -L 7 \
+                --search 'sort:updated-desc involves:@me -assignee:@me' \
+                --json number,title,assignees,updatedAt \
+                --template '{{range .}}{{tablerow .number .title (join "," (pluck "login" .assignees)) (timeago .updatedAt)}}{{end}}{{tablerender}}'
+              ]],
               key = "i",
               action = function()
                 vim.fn.jobstart("gh issue list --web", { detach = true })
@@ -104,12 +74,12 @@ return {
               icon = " ",
               title = "Git Status",
               cmd = "git --no-pager diff --stat -B -M -C",
-              height = 10,
+              height = 7,
             },
           }
           return vim.tbl_map(function(cmd)
             return vim.tbl_extend("force", {
-              pane = 2,
+              pane = 1,
               section = "terminal",
               enabled = in_git,
               padding = 1,
