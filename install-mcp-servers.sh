5ec2869a76a345aed9908bb02f722fb5a6db44ed
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-05-02 15:55:29 +1000

    refactor mcp server setup with json configuration files

--- install-mcp-servers.sh
+++ install-mcp-servers.sh
@@ -6,36 +6,9 @@ set -o nounset
 
 # install mcp servers
 
-claude mcp add \
-  --transport stdio \
-  --scope user \
-  mcp-stellar-xdr-json \
-  -- \
-  npx deno run \
-  --allow-read \
-  https://github.com/leighmcculloch/mcp-stellar-xdr-json/raw/refs/heads/main/mcp-stellar-xdr-json.ts
-
-claude mcp add \
-  --transport stdio \
-  --scope user \
-  mcp-gemini \
-  -- \
-  npx deno run \
-  --allow-read \
-  --allow-env \
-  --allow-net=generativelanguage.googleapis.com:443 \
-  https://github.com/leighmcculloch/mcp-gemini/raw/refs/heads/main/mcp-gemini.ts
-
-claude mcp add \
-  --transport stdio \
-  --scope user \
-  mcp-github \
-  -- \
-  go run github.com/github/github-mcp-server/cmd/github-mcp-server@latest stdio
-
-claude mcp add \
-  --transport stdio \
-  --scope user \
-  mcp-perplexity \
-  -- \
-  npx -y server-perplexity-ask
+pushd mcp
+for f in *.json; do
+  name="${f%.*}"
+  claude mcp remove --scope user "$name" || true
+  claude mcp add-json --scope user "$name" "$(jq --compact-output '.' $f)"
+done
