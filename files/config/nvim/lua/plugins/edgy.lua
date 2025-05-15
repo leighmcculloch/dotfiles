61bafaf5d3f0d15e22cefdf5ef8457597f1c0773
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-05-15 22:47:21 +1000

    update neovim config and lazy-lock.json

--- files/config/nvim/lua/plugins/edgy.lua
+++ files/config/nvim/lua/plugins/edgy.lua
@@ -1,24 +1,42 @@
 return {
   "folke/edgy.nvim",
-  opts = {
-    --right = {
-    --  {
-    --    title = "Outline",
-    --    ft = "Outline",
-    --    pinned = true,
-    --    open = "Outline",
-    --  },
-    --  --For CopilotC-Nvim/CopilotChat.nvim
-    --  --{
-    --  --  ft = "copilot-chat",
-    --  --  title = "Copilot Chat",
-    --  --  size = { width = 50 },
-    --  --  pinned = true,
-    --  --  open = "CopilotChat",
-    --  --},
-    --},
-    animate = {
+  opts = function(_, opts)
+    local step = 15
+    opts.keys = {
+      -- increase width
+      ["<c-<>"] = function(win)
+        win:resize("width", step)
+      end,
+      -- decrease width
+      ["<c->>"] = function(win)
+        win:resize("width", 0 - step)
+      end,
+    }
+    opts.animate = {
       enabled = false,
-    },
-  },
+    }
+  end,
+  --opts = {
+  --  --right = {
+  --  --  {
+  --  --    title = "Outline",
+  --  --    ft = "Outline",
+  --  --    pinned = true,
+  --  --    open = "Outline",
+  --  --  },
+  --  --  --For CopilotC-Nvim/CopilotChat.nvim
+  --  --  --{
+  --  --  --  ft = "copilot-chat",
+  --  --  --  title = "Copilot Chat",
+  --  --  --  size = { width = 50 },
+  --  --  --  pinned = true,
+  --  --  --  open = "CopilotChat",
+  --  --  --},
+  --  --},
+  --  animate = {
+  --    enabled = false,
+  --  },
+  --  
+  --  end
+  --},
 }
