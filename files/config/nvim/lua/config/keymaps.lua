834dc7835c01966578bf2aa86b9c8e480fdefaac
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-06-18 21:57:28 +1000

    update ctrl-p keybinding for file picker to open cwd

--- files/config/nvim/lua/config/keymaps.lua
+++ files/config/nvim/lua/config/keymaps.lua
@@ -7,7 +7,7 @@ local map = vim.keymap.set
 map("n", "<c-h>", "<s-h>", { remap = true, desc = "Change to left buffer" })
 map("n", "<c-l>", "<s-l>", { remap = true, desc = "Change to right buffer" })
 
-map("n", "<c-p>", "<leader> ", { remap = true, desc = "Open file picker" })
+map("n", "<c-p>", "<leader>fF", { remap = true, desc = "Open file picker" })
 map("n", "<c-q>", "<leader>bD", { remap = true, desc = "Close buffer and window" })
 map("n", "<c-e>", "<leader>e", { remap = true, desc = "Reveal file in file explorer" })
 
