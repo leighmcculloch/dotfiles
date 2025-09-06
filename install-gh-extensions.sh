3a75b8438280fe164e5c7eeea57497fb1a80131f
Leigh <351529+leighmcculloch@users.noreply.github.com>
2025-09-07 01:16:46 +1000

    remove gh-dash extension installation

--- install-gh-extensions.sh
+++ install-gh-extensions.sh
@@ -8,4 +8,3 @@ set -o nounset
 gh extension install github/gh-models
 gh extension install github/gh-copilot
 gh extension install meiji163/gh-notify --pin 556df2eecdc0f838244a012759da0b76bcfeb2e7
-gh extension install dlvhdr/gh-dash --pin 605b25ac07928bfc13cde36c312a506d5bb01779 # v4.14.0
