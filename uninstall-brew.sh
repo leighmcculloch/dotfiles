f84fc67551e5218efaee7eb12b4628e157287b57
Leigh <351529+leighmcculloch@users.noreply.github.com>
2025-12-06 00:31:43 +1000

    add documentation and comments to uninstall script

--- uninstall-brew.sh
+++ uninstall-brew.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Uninstall all Homebrew packages.
+# Useful when migrating to a new machine or cleaning up before reinstalling.
+
+# Get all installed packages (formulae and casks)
 packages=$(brew list)
+
+# Uninstall each package one by one
 for pkg in $packages; do
+  # Uncomment below to skip specific packages
   #if [[ "$pkg" == "starship" ]]; then
   #  continue
   #fi
