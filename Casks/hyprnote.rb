9bedf5e6d06d87bc02e3a5becf15b1a23d319299
Leigh <351529+leighmcculloch@users.noreply.github.com>
2025-12-17 11:39:19 +1000

    update hyprnote to 1.0.0-nightly.21

--- Casks/hyprnote.rb
+++ Casks/hyprnote.rb
@@ -1,6 +1,6 @@
 cask "hyprnote" do
-  version "1.0.0-nightly.17"
-  sha256 "4b66fbe1879a488e206c1c1bb3356a57d3a8ea93de50216efdf0c11b1a75754f"
+  version "1.0.0-nightly.21"
+  sha256 "701fa1f55d4c6110b1c7365c6c5671afab97b007ceddb651520ad304d5215e8f"
 
   url "https://github.com/fastrepl/hyprnote/releases/download/desktop_v#{version}/hyprnote-macos-aarch64.dmg"
   name "Hyprnote"
