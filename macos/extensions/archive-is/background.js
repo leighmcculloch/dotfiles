dda4dc70030f6600279cddb47f195e04c675cb6c
Leigh <351529+leighmcculloch@users.noreply.github.com>
2026-07-24 14:48:51 +0000

    strip macos setup to brew, zsh, claude, nvim

--- macos/extensions/archive-is/background.js
+++
@@ -1,6 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  if (tab.url && !tab.url.startsWith("https://archive.is/")) {
-    const archiveUrl = "https://archive.is/" + tab.url;
-    chrome.tabs.update(tab.id, { url: archiveUrl });
-  }
-});
